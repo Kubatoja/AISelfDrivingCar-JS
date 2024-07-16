3f0b0d9a5488fa168fa05e5e89a19f2b495a1f6b
Kubatoja <jakub1720@gmail.com>
2024-07-16 20:01:12 +0200

    Game Final Update

---
+++ car.js
@@ -0,0 +1,157 @@
+class Car {
+  constructor(x, y, width, height, controlType, maxSpeed = 1.5) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = maxSpeed;
+    this.friction = 0.05;
+    this.angle = 0;
+    this.damaged = false;
+    this.controlType = controlType;
+
+    if (controlType != "DUMMY") {
+      this.sensor = new Sensor(this);
+    }
+
+    this.controls = new Controls(controlType);
+
+    this.imgPlayer = new Image();
+    this.imgDUMMY = new Image();
+    this.imgDamaged = new Image();
+    this.imgLoaded = false;
+    Promise.all([
+      new Promise((resolve) => {
+        this.imgPlayer.onload = resolve;
+        this.imgPlayer.src = "porshePLAYER.png"; // Set the correct path
+      }),
+      new Promise((resolve) => {
+        this.imgDUMMY.onload = resolve;
+        this.imgDUMMY.src = "porsheDUMMY.png"; // Set the correct path
+      }),
+      new Promise((resolve) => {
+        this.imgDamaged.onload = resolve;
+        this.imgDamaged.src = "porsheDamaged.png"; // Set the correct path
+      }),
+    ]).then(() => {
+      this.imgLoaded = true;
+      console.log("Images loaded");
+    });
+  }
+
+  update(roadBorders, traffic) {
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders, traffic);
+    }
+    if (this.sensor) this.sensor.update(roadBorders, traffic);
+  }
+
+  #assessDamage(roadBorders, traffic) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+    for (let i = 0; i < traffic.length; i++) {
+      if (polysIntersect(this.polygon, traffic[i].polygon)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+    return points;
+  }
+  #move() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.01 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.01 * flip;
+      }
+    }
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  draw(ctx, color) {
+    if (!this.polygon) return;
+
+    ctx.beginPath();
+
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
+
+    if (this.imgLoaded) {
+      ctx.save();
+
+      ctx.translate(this.x, this.y);
+
+      ctx.rotate(-this.angle);
+      if (this.damaged) {
+        ctx.drawImage(this.imgDamaged, -17.5, -35, 35, 70);
+      } else {
+        if (this.controlType != "DUMMY") {
+          ctx.drawImage(this.imgPlayer, -17.5, -35, 35, 70);
+        } else {
+          ctx.drawImage(this.imgDUMMY, -17.5, -35, 35, 70);
+        }
+      }
+
+      ctx.restore();
+    }
+    if (this.sensor) this.sensor.draw(ctx);
+  }
+}
